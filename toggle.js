da82554fcfec44b0285ac1ef7be1a3f72a655262
herraphfx <herraph@gmail.com>
2022-04-12 14:52:22 +0400

    CV Updated

---
+++ toggle.js
@@ -0,0 +1,6 @@
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarNavAltMarkup')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
